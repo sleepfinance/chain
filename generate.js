13f69f57bd64a05a8cdc7aa4d92010b9a1477a69
Zamba Izo <ofuzak@gmail.com>
2023-01-17 13:39:20 +0300

    Fix install in js projects

--- generate.js
+++ generate.js
@@ -1,11 +1,10 @@
-import { Chain, ChainId } from '.'
-import fetch from 'node-fetch'
-import { writeFileSync } from 'fs'
+const { Chain, ChainId } = require('./dist')
+const fetch = require('node-fetch') 
+const { writeFileSync } = require('fs') 
 
 async function generate() {
-  const allChains = await fetch('https://chainid.network/chains.json').then((data) => data.json() as Promise<Chain[]>)
+  const allChains = await fetch('https://chainid.network/chains.json').then((data) => data.json())
   const filteredChains = allChains.filter(({ chainId }) => Object.values(ChainId).includes(chainId))
-
   writeFileSync('./chains.ts', `export default ${JSON.stringify(filteredChains)} as const`)
   process.exit(0)
 }
